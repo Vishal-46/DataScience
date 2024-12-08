a94056da430a4b0fe5e40879c6c160ac31ff374d
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-12-08 18:46:05 +0530

    Update GameSales.py

--- GameSales.py
+++ GameSales.py
@@ -1,2 +1,3 @@
 #Here I will be Practicing EDA on game sales CSV dataset. each and every basic steps will be practiced and uploaded more than once a week.
 
+#First, numpy, pandas, and matplotlib are imported, then the CSV is loaded into a DataFrame and its structure.
