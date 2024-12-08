f827cb571bfbbf2d61a4206ea80bb5eb22a400a3
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-12-08 19:48:12 +0530

    Update GameSales.py

--- GameSales.py
+++ GameSales.py
@@ -1,6 +1,6 @@
 #Here I will be Practicing EDA on game sales CSV dataset. each and every basic steps will be practiced and uploaded more than once a week.
 
-#First, numpy, pandas, and matplotlib are imported, then the CSV is loaded into a DataFrame and its structure.
+#First, numpy, pandas, seaborn and matplotlib are imported, then the CSV is loaded into a DataFrame and its structure.
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -10,6 +10,8 @@ print(data.head())
 print(data.info())
 print(data.describe())
 #Here The information of the dataset like no. of columns, what are all the columns their datatype and also its quantity are shown.
+#After this let us try to visualize a graph. Taking Year column and lets try to visualize it in a histogram.Histogram can be visualized using seaborn
+sns.histplot(data['Year'], bins=30) #here there will be a histogram(graph) with column year in the x axis and count on the y axis.
 
 ---
 #
