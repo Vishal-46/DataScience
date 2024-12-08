c10d96da2f6ddd00bf11205910842882008d41f9
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-12-08 18:47:07 +0530

    Update GameSales.py

--- GameSales.py
+++ GameSales.py
@@ -1,3 +1,12 @@
 #Here I will be Practicing EDA on game sales CSV dataset. each and every basic steps will be practiced and uploaded more than once a week.
 
 #First, numpy, pandas, and matplotlib are imported, then the CSV is loaded into a DataFrame and its structure.
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+data=pd.read_csv('C:/Users/visha/Downloads/video-game-sales.csv')
+print(data.head())
+print(data.info())
+print(data.describe())
+---
+#
