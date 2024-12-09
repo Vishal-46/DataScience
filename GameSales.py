5f192e158401fd40438c17626126ff67504df0e2
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-12-09 07:50:20 +0530

    Update GameSales.py

--- GameSales.py
+++ GameSales.py
@@ -13,6 +13,9 @@ print(data.describe())
 #After this let us try to visualize a graph. Taking Year column and lets try to visualize it in a histogram.Histogram can be visualized using seaborn
 sns.histplot(data['Year'], bins=30) #here there will be a histogram(graph) with column year in the x axis and count on the y axis.
 #data cleaning, First step in Data cleaning is checking the missing values of the dataset for that we use dataset name.isna().sum().
-print(data.isna().sum()
+print(data.isna().sum())
+#when the dataframe or dataset has missing values
+data.dropna(inplace=True)  # Removes all rows with ANY missing values.
+
 ---
 #
